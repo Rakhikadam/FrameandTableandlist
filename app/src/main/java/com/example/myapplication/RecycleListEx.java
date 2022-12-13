b9f3504d0f5409a3d3813725e741d8e5368e7ff1
Rakhikadam <Rakhikadam03@gmail.com>
2022-12-13 17:55:06 +0530

    RecycleListEx add

--- app/src/main/java/com/example/myapplication/RecycleListEx.java
+++ app/src/main/java/com/example/myapplication/RecycleListEx.java
@@ -2,11 +2,17 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +28,9 @@ public class RecycleListEx extends AppCompatActivity {
         List<contactlist>list = new ArrayList<>();
         list.add(new contactlist("rakhi","865542","rakhi1@gmail.com"));
         list.add(new contactlist("rani","9933332","rani1@gmail.com"));
+        ContactAdpter adpter = new ContactAdpter(list);
+        contact.setLayoutManager(new LinearLayoutManager(RecycleListEx.this));
+        contact.setAdapter(adpter);
 
 
     }
@@ -30,27 +39,75 @@ public class RecycleListEx extends AppCompatActivity {
     //after method override
 
     class ContactAdpter extends RecyclerView.Adapter<ContactAdpter.MyCustomViewAdpter>{
+        List<contactlist>list1;
+
+
+        public ContactAdpter(List<contactlist> list) {
+            this.list1 = list;
+
+        }
 
         @NonNull
         @Override
         public ContactAdpter.MyCustomViewAdpter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View view = LayoutInflater.from(RecycleListEx.this).inflate(R.layout.call_list_recycle,parent,false);
+            MyCustomViewAdpter holder = new MyCustomViewAdpter(view);
+            return holder;
         }
 
         @Override
         public void onBindViewHolder(@NonNull ContactAdpter.MyCustomViewAdpter holder, int position) {
+            holder.name.setText(list1.get(position).getName());
+            holder.mail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent mailIntent = new Intent(Intent.ACTION_VIEW);
+                    mailIntent.setData(Uri.parse("mailto:"+list1.get(position).getEmail()));
+                    startActivity(mailIntent);
+                }
+            });
+
+            holder.number.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:"+list1.get(position).getNumber()));
+                    startActivity(callIntent);
+                }
+            });
+
+            holder.sms.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent msgIntent = new Intent(Intent.ACTION_VIEW);
+                    msgIntent.setData(Uri.parse("sms:"+list1.get(position).getNumber()));
+                    startActivity(msgIntent);
+                }
+            });
+
 
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return list1.size();
+
         }
 
         public class MyCustomViewAdpter extends RecyclerView.ViewHolder {
+            TextView name , mail;
+            ImageView number , sms;
+
+
             public MyCustomViewAdpter(@NonNull View itemView) {
                 super(itemView);
+                name = itemView.findViewById(R.id.nameRecycle);
+                mail =itemView.findViewById(R.id.mailRecycle);
+                number =itemView.findViewById(R.id.callRecycle);
+                sms = itemView. findViewById(R.id.msgRecycle);
+
             }
+
         }
     }
 }
